adf0c43e8f9d7718f9c8513f5a0eaebb17b43dfb
INOCCENT-Dev <105031200+INOCCENT-Dev@users.noreply.github.com>
2022-12-29 01:53:27 +0900

    Add files via upload

--- code.js
+++ code.js
@@ -1,5 +1,19 @@
 window.onload = function(){
   let mouse = document.getElementById("mouse");
+
+  let observer = new IntersectionObserver((e)=>{
+    e.forEach((box)=>{
+      if(box.isIntersecting){
+        box.target.style.opacity = 1;
+      }else{
+        box.target.style.opacity = 0;
+      }
+    })
+  });
+
+  let info = document.querySelectorAll('.info_text p');
+
+  observer.observe(info[0]);
   
   window.addEventListener("scroll",function(){
     mouse.style.left = (window.scrollY/(document.documentElement.scrollHeight - (window.innerHeight*0.7))) * window.innerWidth +10+'px';
@@ -7,8 +21,8 @@ window.onload = function(){
   
   document.getElementById("intro").addEventListener('click',function(){movePage(0)});
   document.getElementById("nav").addEventListener('click',function(){movePage(1150)});
-  document.getElementById("fund").addEventListener('click',function(){movePage(2000)});
-  document.getElementById("credit").addEventListener('click',function(){movePage(3000)});
+  document.getElementById("fund").addEventListener('click',function(){movePage(2100)});
+  document.getElementById("credit").addEventListener('click',function(){movePage(3200)});
 }
 
 function movePage(location){
